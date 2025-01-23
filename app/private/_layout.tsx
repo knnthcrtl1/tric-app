985f9477116eb0fc8a66b695325edc5a57bf084b
knnthcrtl1 <knnthcrtl.kc@gmail.com>
2025-01-24 00:06:26 +0800

    setup tabs for book and settings

--- app/private/_layout.tsx
+++ app/private/_layout.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import { Stack } from 'expo-router';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function PrivateLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-      <Stack >
-        <Stack.Screen name="home" options={{ headerShown: false }} />
-        <Stack.Screen name="book" options={{ headerShown: false }} />
-      </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="book"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="settings.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ app/private/settings.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BookingScreen from "@/screens/BookingScreen";
+
+const Settings = () => {
+  return <BookingScreen />;
+};
+
+export default Settings;
